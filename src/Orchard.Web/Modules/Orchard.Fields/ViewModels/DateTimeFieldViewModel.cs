fd839ac6dd7f3323eaf04736eaf88f3df75575a1
qinzheng xu <xqz101361@163.com>
2016-12-15 20:39:28 +0800

    测试网络提交

---
+++ src/Orchard.Web/Modules/Orchard.Fields/ViewModels/DateTimeFieldViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Orchard.Core.Common.ViewModels;
+
+namespace Orchard.Fields.ViewModels {
+    public class DateTimeFieldViewModel {
+        public string Name { get; set; }
+        public string Hint { get; set; }
+        public bool IsRequired { get; set; }
+        public DateTimeEditor Editor { get; set; }
+        public bool HasDefaultValue { get; set; }
+    }
+}
